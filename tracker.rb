5ed015c3fd95318d1006716938ac216fc4d33159
Scott Dudley & J.J. Idt <student@epicodus.com>
2014-08-13 16:54:23 -0700

    add UI

--- tracker.rb
+++ tracker.rb
@@ -1,3 +1,14 @@
+require 'active_record'
+require 'pry'
+
+require './lib/division'
+require './lib/employee'
+require './lib/project'
+require './lib/employee_project'
+
+ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))["development"])
+
+
 def ws
   puts ""
 end
@@ -7,12 +18,62 @@ def prompt(text)
   gets.chomp
 end
 
+def clear
+  system('clear')
+end
+
 def header
-puts "****************
-      Employee Tracker
-      ****************"
+puts "****************\n" +
+     "Employee Tracker\n" +
+     "****************\n"
 end
 
 def separator
   puts "##################"
 end
+
+def main_menu
+  header
+  puts "Select choice A > add employee"
+  puts "Select choice D > add division"
+  puts "Select choice AS > assign employee to division"
+
+  case gets.chomp.upcase
+  when 'A'
+    add_employee
+    clear
+    main_menu
+  when 'D'
+    add_division
+    clear
+    main_menu
+  when 'AS'
+    list_employees
+    list_divisions
+    gets.chomp
+    clear
+    main_menu
+  end
+end
+
+def add_employee
+  Employee.create(:name => prompt("Enter employee's name"))
+end
+
+def add_division
+  Division.create(:name => prompt("Enter division's name"))
+end
+
+def list_employees
+  puts "EMPLOYEES"
+  separator
+  Employee.all.each { |employee| puts "*" + employee.name}
+end
+
+def list_divisions
+  puts "DIVISIONS"
+  separator
+  Division.all.each { |division| puts "*" + division.name}
+end
+
+main_menu
